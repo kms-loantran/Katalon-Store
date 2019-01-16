7dff6e882465a7f804509b853a300f2a499e093c
Loan Tran <loantran@C02TM1AYGVC8.local>
2019-01-16 11:19:27 +0700

    Katalon Store

---
+++ Object Repository/Home/lnkSignIn.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lnkSignIn</name>
+   <tag></tag>
+   <elementGuidId>52c53c57-2f16-4e54-bcba-f1f253366b6d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@class = 'nav-link waves-effect waves-light']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[text()='Sign in ']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>nav-link waves-effect waves-light</value>
+   </webElementProperties>
+</WebElementEntity>
